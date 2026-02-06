3fc9eb43689b4e5610fa348ea059fe5a5478a3f6
ArghgrA <0xarghh@gmail.com>
2026-02-06 22:13:29 +0100

    Ho modificato la classe DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	//Ho modificato la seguente classe
+	//linea cambiata
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
+
+
+
